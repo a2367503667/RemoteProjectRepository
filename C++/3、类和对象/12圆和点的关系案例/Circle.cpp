918457593de71698de1696146ecf776ab660a14b
yuangeGit <2367503667@qq.com>
2024-06-03 15:45:44 +0800

    Circle.cpp v1.0版本

---
+++ C++/3、类和对象/12圆和点的关系案例/Circle.cpp
@@ -0,0 +1,18 @@
+#include "Point.h"
+
+void Point::setX(double x)
+{
+	this->x = x;
+}
+double Point::getX()
+{
+	return this->x;
+}
+void Point::setY(double y)
+{
+	this->y = y;
+}
+double Point::getY()
+{
+	return this->y;
+}
