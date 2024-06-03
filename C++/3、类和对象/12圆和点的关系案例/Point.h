540dab809f055db32cbeaf72b1f0419842f5c82a
yuangeGit <2367503667@qq.com>
2024-06-03 15:46:56 +0800

    Point.h v1.0版本

---
+++ C++/3、类和对象/12圆和点的关系案例/Point.h
@@ -0,0 +1,14 @@
+#ifndef __POINT_H__
+#define __POINT_H__
+class Point
+{
+public:
+	void setX(double x);
+	double getX();
+	void setY(double y);
+	double getY();
+private:
+	double x;
+	double y;
+};
+#endif
